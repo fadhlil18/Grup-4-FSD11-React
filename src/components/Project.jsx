f7e894c318e1b1d4f9b3869dbba9a37b2e9ea16c
fadhlil18 <fadhlilazfi@gmail.com>
2024-05-23 06:15:31 +0700

    fixing responsive

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,9 +4,9 @@ import data from '../mocks/projects.json';
 
 const Project = () => {
   return (
-    <div className='relative'>
+    <div id="Project" className='relative'>
       <h2 className="text-3xl font-bold mb-8 mt-4 text-center">My Projects</h2>
-      <div id="Project" className="flex flex-col xl:flex-row items-center justify-center mb-12 max-w-7xl mx-auto">
+      <div className="flex flex-col xl:flex-row items-center justify-center mb-12 max-w-7xl mx-auto">
         {data.map(item => {
           return (
             <div key={item.id} className="m-4">
