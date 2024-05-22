ae95e9b5a07b7290d167b5a476f294c2a4ce6997
fadhlil18 <fadhlilazfi@gmail.com>
2024-05-23 00:05:55 +0700

    fixed path vercel

--- src/components/Atoms/ImageSlider.jsx
+++ src/components/Atoms/ImageSlider.jsx
@@ -4,10 +4,10 @@ import 'react-slideshow-image/dist/styles.css';
 
 function ImageSlider({ data }) {
     if (data.length < 2) {
-        return <div><img className="rounded-lg object-cover flex justify-center bg-cover" src={data} alt="img-card" /></div>
+        return <div><img className="rounded-lg object-cover flex justify-center bg-cover" src={data} alt={data} /></div>
     } else {
         return (
-            <div className="w-[350px] sm:w-[350px] m-[2px] md:w-[650px]">
+            <div className=" m-[2px] w-[350px] sm:w-[550px] md:w-[650px] lg:w-[650px]">
                 <Slide autoplay={false}>
                     {data.map((each, index) => (
                         <div key={index}>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Slideshow from '../components/Atoms/ImageSlider';
-import data from './Projects.json';
+import data from '../mocks/projects.json';
 
 const Project = () => {
   return (
     <div className='relative'>
       <h2 className="text-3xl font-bold mb-8 mt-4 text-center">My Projects</h2>
-      <div id="Project" className="flex flex-col lg:flex-row items-center justify-center mb-12 max-w-7xl mx-auto">
+      <div id="Project" className="flex flex-col xl:flex-row items-center justify-center mb-12 max-w-7xl mx-auto">
         {data.map(item => {
           return (
             <div key={item.id} className="m-4">
-              <div className="flex flex-col items-center lg:flex-col lg:items-start">
+              <div className="flex flex-col items-center  lg:flex-col lg:items-center">
                 <Slideshow data={item.picture} className="mb-2"/>
                 <div className="text-center mt-4 lg:text-left lg:ml-4">
                   <div className="text-center font-bold text-xl">{item.name}</div>
